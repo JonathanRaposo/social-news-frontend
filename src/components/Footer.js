9d7f064f242056df190e77d536b2d7479d02efa5
Jonathan Raposo <jonathan.raposo.luna@gmail.com>
2022-12-15 19:33:13 -0500

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -18,12 +18,12 @@ const Footer = () => {
                 <li>
                     <Link to="/about">About</Link>
                 </li>
-                <li>
+                {/* <li>
                     <Link to="/about">Contact</Link>
                 </li>
                 <li>
                     <Link>Support</Link>
-                </li>
+                </li> */}
 
 
             </ul>
