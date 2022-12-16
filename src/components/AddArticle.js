ddf16287e36c64ce03e6d1f8913b261e524483b8
Jonathan Raposo <jonathan.raposo.luna@gmail.com>
2022-12-15 19:54:24 -0500

    add new default image

--- src/components/AddArticle.js
+++ src/components/AddArticle.js
@@ -2,6 +2,7 @@ import { AuthContext } from '../context/auth.context';
 import { useContext, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import planet from '../assets/images/planet.jpeg'
 
 const API_URL = 'https://pink-doubtful-hen.cyclic.app';
 
@@ -56,7 +57,8 @@ const AddArticle = ({ oneArticle }) => {
     <div className={'AddArticle ' + theme}>
 
       {image && <img src={oneArticle?.image?.thumbnail?.contentUrl} alt="oneArticle" />}
-      {!image && <p className="no-image-para">No thumbnail available</p>}
+      {/* {!image && <p className="no-image-para">No thumbnail available</p>} */}
+      {!image && <img src={planet} alt="default-pic" />}
 
       <a href={oneArticle?.url} target="_blank" rel="noreferrer">
         <h3>{oneArticle?.name}</h3>
